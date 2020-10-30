8c0aed5ff39e83efaa31ad3b1e6bcf818e309210
yamin <yaminkhan017@gmail.com>
2020-10-30 10:42:45 +0600

    first

---
+++ lib/note_list.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:sqlite_demo/note.dart';
+import 'package:sqlite_demo/providers/note_provider.dart';
+
+class NoteList extends StatefulWidget {
+  @override
+  _NoteListState createState() => _NoteListState();
+}
+
+class _NoteListState extends State<NoteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+      ),
+      body: FutureBuilder(
+        future: NoteProvider.getNoteList(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final notes = snapshot.data;
+            return ListView.builder(
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                Note(NoteMode.Editing, notes[index])));
+                  },
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.only(
+                          top: 30.0, bottom: 30, left: 13.0, right: 22.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          _NoteTitle(
+                            title: notes[index]['title'],
+                          ),
+                          Container(
+                            height: 4,
+                          ),
+                          _NoteText(
+                            text: notes[index]['text'],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+              itemCount: notes.length,
+            );
+          }
+          return Center(child: CircularProgressIndicator());
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => Note(NoteMode.Adding, null),
+            ),
+          );
+        },
+        child: Icon(
+          Icons.add,
+        ),
+      ),
+    );
+  }
+}
+
+class _NoteText extends StatelessWidget {
+  final String text;
+  _NoteText({@required this.text});
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(color: Colors.grey.shade800),
+      maxLines: 3,
+      overflow: TextOverflow.ellipsis,
+    );
+  }
+}
+
+class _NoteTitle extends StatelessWidget {
+  final String title;
+  _NoteTitle({@required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+    );
+  }
+}
